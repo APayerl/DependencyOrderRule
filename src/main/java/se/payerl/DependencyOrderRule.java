9e821d5c69b1f1856f3d8fb9ac9bea33a2f130be
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2024-05-08 08:15:00 +0200

    change strings in Named annotation to camelCase

--- src/main/java/se/payerl/DependencyOrderRule.java
+++ src/main/java/se/payerl/DependencyOrderRule.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Named("DependencyOrderRule")
+@Named("dependencyOrderRule")
 public class DependencyOrderRule extends AbstractEnforcerRule {
     @Inject
     private MavenProject project;
